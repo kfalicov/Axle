3bec5860d8b711179e005726b2fd9cd5614c117b
Craig Ferris <cmferris97@yahoo.com>
2017-12-15 17:28:12 -0500

    Added project files
    -Added libraries GLFW, SOIL, GLM

---
+++ FFW-Axle/FFW-Axle/main.cpp
@@ -0,0 +1,13 @@
+
+#include <glm/glm.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+
+int main()
+{
+	printf("Hello World");
+	return 0;
+}
